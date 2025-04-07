0bfd173792be09c2dc24fbb81828ab2b934315fc
IgorKovalev07 <iigorkovalev@gmail.com>
2025-04-07 14:36:00 +0300

    new project

---
+++ untitled/src/main/java/web/service/UserService.java
@@ -0,0 +1,43 @@
+package web.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    private UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    public void updateUser(Long id, User user) {
+        userDao.updateUser(id, user);
+    }
+
+
+    public User showUser(Long id) {
+        return userDao.showUser(id);
+    }
+}
